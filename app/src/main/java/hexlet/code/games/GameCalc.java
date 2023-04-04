38476b99ee464419a2a94e7505f5ec57032dc1e9
Arthur <toatodd@gmail.com>
2023-04-04 18:21:18 +0300

    Match last comments

--- app/src/main/java/hexlet/code/games/GameCalc.java
+++ app/src/main/java/hexlet/code/games/GameCalc.java
@@ -9,6 +9,19 @@ public class GameCalc {
     private static final int MIN_RANDOM_NUMBER = 1;
     private static final char[] OPERATORS = {'+', '-', '*'};
 
+    public static int calculate(int number1, int number2, char operator) {
+        switch (operator) {
+            case '+':
+                return number1 + number2;
+            case '-':
+                return number1 - number2;
+            case '*':
+                return number1 * number2;
+            default:
+                throw new Error("Unknown operator: " + operator);
+        }
+    }
+
     public static void calc() {
         String[][] data = new String[Engine.ROUNDS_COUNT][2];
 
@@ -18,7 +31,7 @@ public class GameCalc {
             var num2 = Utils.generateNumber(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER);
 
             data[i][0] = "" + num1 + " " + operator + " " + num2;
-            data[i][1] = Integer.toString(Utils.calculate(num1, num2, operator));
+            data[i][1] = Integer.toString(calculate(num1, num2, operator));
         }
 
         Engine.game(data, TASK);
